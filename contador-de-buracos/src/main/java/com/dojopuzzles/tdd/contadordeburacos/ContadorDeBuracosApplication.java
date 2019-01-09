72c466195f665d9c630feee4397a95d535905a48
leonardo-anjos <leodrums11@gmail.com>
2019-01-09 18:06:58 -0300

    dojo puzzles - contar buracos

---
+++ contador-de-buracos/src/main/java/com/dojopuzzles/tdd/contadordeburacos/ContadorDeBuracosApplication.java
@@ -0,0 +1,14 @@
+package com.dojopuzzles.tdd.contadordeburacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContadorDeBuracosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContadorDeBuracosApplication.class, args);
+	}
+
+}
+
---
+++ contador-de-buracos/src/test/java/com/dojopuzzles/tdd/contadordeburacos/ContadorDeBuracosApplicationTests.java
@@ -0,0 +1,58 @@
+package com.dojopuzzles.tdd.contadordeburacos;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContadorDeBuracosApplicationTests {
+
+	@Test
+	public void Palavra_Com_Letra_C_Sem_Buraco_Rotorna_0() {
+		String palavra = "C";
+		VerificadorDeBuraco vdb = new VerificadorDeBuraco();
+		int cont = vdb.quantidadeDeBuracos(palavra);
+
+		Assert.assertEquals(0, cont);
+	}
+
+	@Test
+	public void Palavra_Com_Letra_A_Retorna_1() {
+		String palavra = "A";
+		VerificadorDeBuraco vdb = new VerificadorDeBuraco();
+		int cont = vdb.quantidadeDeBuracos(palavra);
+
+		Assert.assertEquals(1, cont);
+	}
+
+	@Test
+	public void Palavra_Com_Letra_B_Retorna_2() {
+		String palavra = "B";
+		VerificadorDeBuraco vdb = new VerificadorDeBuraco();
+		int cont = vdb.quantidadeDeBuracos(palavra);
+
+		Assert.assertEquals(2, cont);
+	}
+
+	@Test
+	public void Palavra_Com_Letra_A_e_B_Retorna_3() {
+		String palavra = "AB";
+		VerificadorDeBuraco vdb = new VerificadorDeBuraco();
+		int cont = vdb.quantidadeDeBuracos(palavra);
+
+		Assert.assertEquals(3, cont);
+	}
+
+	@Test
+	public void Palavra_Com_2_Letras_B_Retorna_4() {
+		String palavra = "BB";
+		VerificadorDeBuraco vdb = new VerificadorDeBuraco();
+		int cont = vdb.quantidadeDeBuracos(palavra);
+
+		Assert.assertEquals(4, cont);
+	}
+
+}
